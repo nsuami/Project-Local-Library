51209fb5ac8629efaa8ca9391cc99f13dab5b603
nsuami <2nsuami@gmail.com>
2022-05-15 11:25:08 -0400

    Complete qualified assignment, ...

--- public/src/home.js
+++ public/src/home.js
@@ -1,67 +1,51 @@
-/*Using the .length array method to return the number of elements (book objects) 
-in the books array.*/
-function getTotalBooksCount(books) {
- return books.length;
-}
-/*Using the .length array method to return the number of elements (accounts objects) 
-in the accounts array.*/
-function getTotalAccountsCount(accounts) {
- return accounts.length;
-}
+const {
+  findAuthorById,
+} = require('./books.js');
 
+function getTotalBooksCount(books){
+  return books.reduce((accumilator, book) => accumilator + 1, 0);
+};
+
+function getTotalAccountsCount(accounts) {
+ return accounts.reduce((accumilator, user) => accumilator + 1, 0);
+}
 
 function getBooksBorrowedCount(books) {
- let booksCheckedOut = books.filter(
-  (book) =>
-   book.borrows.filter((record) => record.returned === false).length > 0
- );
- return booksCheckedOut.length;
+  return books.filter((book) => book.borrows[0].returned === false).length;
+}
+
+function sortAndGiveTopFive(array) {
+  return array.sort((itemA, itemB) => (itemA.count < itemB.count ? 1 : -1)).slice(0, 5);
 }
 
 function getMostCommonGenres(books) {
- let map = {};
- books.forEach((num) => {
-  if (map[num.genre]) {
-   map[num.genre]++;
-  } else {
-   map[num.genre] = 1;
-  }
- });
- return Object.entries(map)
-  .map(([name, count]) => {
-   return {
-    name,
-    count
-   };
-  })
-  .sort((a, b) => b.count - a.count)
-  .slice(0, 5);
+  const result = books.reduce((acc, book) => {
+    const existingGenre = acc.find((genre) => genre.name === book.genre);
+    // eslint-disable-next-line no-unused-expressions
+    existingGenre ? existingGenre.count += 1 : acc.push({ name: book.genre, count: 1 });
+    return acc;
+  }, []);
+  return sortAndGiveTopFive(result);
 }
 
 function getMostPopularBooks(books) {
- return books
-  .map((book) => {
-   return { name: book.title, count: book.borrows.length };
-  })
-  .sort((a, b) => (a.count < b.count ? 1 : -1))
-  .slice(0, 5);
+  const result = books.reduce((acc, book) => {
+    acc.push({ name: book.title, count: book.borrows.length });
+    return acc;
+  }, []);
+  return sortAndGiveTopFive(result);
 }
 
 function getMostPopularAuthors(books, authors) {
- let result = [];
- authors.forEach((author) => {
-  let theAuthor = {
-   name: `${author.name.first} ${author.name.last}`,
-   count: 0
-  };
-  books.forEach((book) => {
-   if (book.authorId === author.id) {
-    theAuthor.count += book.borrows.length;
-   }
-  });
-  result.push(theAuthor);
- });
- return result.sort((a, b) => b.count - a.count).slice(0, 5);
+  const result = books.reduce((acc, book) => {
+    const { name: { first, last } } = findAuthorById(authors, book.authorId);
+    acc.push({
+      name: `${first} ${last}`,
+      count: book.borrows.length,
+    });
+    return acc;
+  }, []);
+  return sortAndGiveTopFive(result);
 }
 
 module.exports = {
